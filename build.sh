3e95ffb6792c9684acaeab1ad040e68e4e16d63e
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-03 00:37:34 -0700

    feat(hunspell): setup dockerfile to use fork

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
+# invokce emscripten to build binary targets configured via dockerfile.
+
 em++ \
-  -O3 \
-  -Oz \
-  --llvm-lto 1 \
-  -s -s WASM=1 \
-  -s NO_EXIT_RUNTIME=1 \
-  -s EXPORTED_FUNCTIONS="['_Hunspell_create', '_Hunspell_destroy', '_Hunspell_spell']" \
-  ./src/hunspell/.libs/libhunspell-1.6.a \
-  -o $1
+-O3 \
+-Oz \
+--llvm-lto 1 \
+-s NO_EXIT_RUNTIME=1 \
+-s EXPORTED_FUNCTIONS="['_Hunspell_create', '_Hunspell_destroy', '_Hunspell_spell']" \
+./src/hunspell/.libs/libhunspell-1.6.a \
+$@

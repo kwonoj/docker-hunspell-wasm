1ed07a94d73470e10e1e9cdb997399379d5b4c04
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-03 11:15:07 -0700

    feat(preprocessor): add preprocessor for node.js

---
+++ preprocessor.js
@@ -0,0 +1,15 @@
+/**
+ * Preprocessor script to be injected into compiled output
+ *
+ * In here, detect environment and if it's node, override `Module.wasmBinaryFile`
+ * to use relative path - by default, it always look for current running directory.
+ * https://github.com/kripken/emscripten/pull/5296 will allow embed everything into
+ * single file, which'll be solution for this.
+ */
+if (typeof module !== 'undefined' && module.exports) {
+  var Module = {};
+  if (typeof __dirname === "string") {
+    //___wasm_binary_name___ is being replaced build time via build.sh
+    Module["wasmBinaryFile"] = require('path').join(__dirname, "___wasm_binary_name___");
+  }
+}
